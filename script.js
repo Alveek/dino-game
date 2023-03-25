ec05510670a7b72c654bfdd550664f4eba10d2d2
mintolime <104442896+mintolime@users.noreply.github.com>
2023-03-25 13:31:50 +0000

    fix: btn exit game

--- script.js
+++ script.js
@@ -5,40 +5,42 @@ const pageGameOver = document.querySelector('.page-final');
 const dino = document.querySelector('#dino');
 const cactus = document.querySelector('#cactus');
 const exitBtn = document.querySelector('.game__btn-quit');
+const backBtn = document.querySelector('.game__btn-back');
 const jumpBtn = document.querySelector('.game__btn-jump');
-const yesBtn = document.querySelector('.game__btn-yes');
-const noneBtn = document.querySelector('.game__btn-no');
-const playAgainBtn = document.querySelector('.game__btn-return');
+const yesBtn = pageStart.querySelector('.game__btn-yes');
+const noneBtn = pageStart.querySelector('.game__btn-no');
+const playAgainBtn = pageGameOver.querySelector('.game__btn-return');
 
-let score = 0; 
+let score = 0;
 
 const fix = (evt) => {
   console.log(evt.target);
 };
 
+//фукнции для отобажения активных страниц
 function activePage(page) {
   page.classList.add('active');
 }
 function inativePage(page) {
   page.classList.remove('active');
 }
-//можно написать через условие 
-function rechangedPage(open,close) {
-  activePage(open)
-  inativePage(close)
+//можно написать через условие
+function rechangedPage(open, close) {
+  activePage(open);
+  inativePage(close);
 }
 
-
+//проверка кнопок при входе
 const checkBtnStart = (evt) => {
   if (evt.target === yesBtn) {
-    rechangedPage(pageGame,pageStart)
-  } else if (evt.target === noneBtn ) {
-    rechangedPage(pageEnd,pageStart)
+    rechangedPage(pageGame, pageStart);
+  } else if (evt.target === noneBtn) {
+    rechangedPage(pageEnd, pageStart);
   }
 };
 
 const jump = () => {
-  if (!dino.classList.contains("jump")) {
+  if (!dino.classList.contains('jump')) {
     dino.classList.add('jump');
   }
   setTimeout(function () {
@@ -49,16 +51,15 @@ const jump = () => {
 let isAlive = setInterval(function () {
   let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
   let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'));
-  if (cactusLeft < 40 && cactusLeft > 0 && dinoTop >= 140) {
-    rechangedPage(pageGameOver,pageGame)
-    console.log(dinoTop)
+  if (cactusLeft < 30 && cactusLeft > 0 && dinoTop >= 140) {
+    rechangedPage(pageGameOver, pageGame);
+    console.log(dinoTop);
   }
-
- 
 });
 
 document.addEventListener('keydown', () => jump());
 document.addEventListener('click', (evt) => checkBtnStart(evt));
 jumpBtn.addEventListener('click', () => jump());
-playAgainBtn.addEventListener('click', () => rechangedPage(pageGame,pageGameOver));
-exitBtn.addEventListener('click', () => rechangedPage(pageGame,pageStart));
+playAgainBtn.addEventListener('click', () => rechangedPage(pageGame, pageGameOver));
+backBtn.addEventListener('click', () => rechangedPage(pageStart, pageEnd));
+exitBtn.addEventListener('click', () =>rechangedPage(pageStart, pageGame));
