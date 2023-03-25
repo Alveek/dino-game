b373f95da0b7fe3754a94f02030e706a8ac52768
mintolime <104442896+mintolime@users.noreply.github.com>
2023-03-25 03:21:17 +0000

    fix: fix jumpBtn

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
 const dino = document.querySelector('#dino');
 const cactus = document.querySelector('#cactus');
 const exitBtn = document.querySelector('.game__btn-quit');
-const jumpBtn = document.querySelector('game__btn-jump');
+const jumpBtn = document.querySelector('.game__btn-jump');
+console.log(jumpBtn)
 
-document.addEventListener("keydown", function(evt) {
-
-  jump();
-});
-
-document.addEventListener("click", function(evt) {
-// console.log(evt.target)
-jump()
-});
 
 // console.log(dino,cactus)
 const jump = (evt) => {
-  if (dino.classList != "jump" || evt.tardet === jumpBtn) {
-    dino.classList.add("jump")
+  if (dino.classList != 'jump' || evt.tardet === jumpBtn) {
+    dino.classList.add('jump');
   }
-  setTimeout( function() {
-    dino.classList.remove("jump")
-  }, 300)
-}  
+  setTimeout(function () {
+    dino.classList.remove('jump');
+  }, 300);
+};
 
 let isAlive = setInterval(function () {
-  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
-  let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
+  let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'));
   if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
-    alert("GAME OVER!!")
+    alert('GAME OVER!!');
   }
-})
+});
+
+document.addEventListener('keydown', () => jump());
+jumpBtn.addEventListener('click', () => jump());
